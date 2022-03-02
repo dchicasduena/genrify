582ea13687e4f29c609f542d5610aa86e373c265
David Chicas <dchicasduena@mun.ca>
2022-03-02 13:50:25 -0330

    tried adding genres

--- Iter Two/random.js
+++ Iter Two/random.js
@@ -10,7 +10,8 @@ let fs = require('fs');
 let randSong = convertDataToSong("spotify_songs.csv");
 
 function convertDataToSong(filename){
-    const song_array = []; // 
+    const song_array = []; 
+    const genre_array = [];
     const data = fs.readFileSync(filename, {encoding:'utf8', flag:'r'}); // open and read file
     const songs = data.split('\n'); // split into each song 
 
@@ -24,12 +25,18 @@ function convertDataToSong(filename){
             playlist_name: song[7], 
             playlist_genre: song[9], 
             playlist_subgenre: song[10],  
-            duration_ms: song[22]
+            duration: parseInt(song[22])
         }
     
+    // change duration to minutes
+    const minutes = Math.floor(songObj.duration / 60000);
+    const seconds = ((songObj.duration % 60000) / 1000).toFixed(0);
+    songObj.duration = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+
     song_array.push(songObj)
     }
-  
-    console.log(song_array[0])
+
+
+
     return song_array;
 }
