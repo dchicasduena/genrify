c7370155652715cdfa1946b063f2098cad55a50e
David Chicas <dchicasduena@mun.ca>
2022-03-02 18:24:39 -0330

    fixed length issue

--- Iter Two/random.js
+++ Iter Two/random.js
@@ -16,15 +16,20 @@ function convertDataToSong(filename){
     const songs = data.split('\n'); // split into each song 
 
     for (let i = 1; i < songs.length; i++){ // go through the songs
-        const song = songs[i].split(','); // split each song into its attributes
+        const song = songs[i].split(/,/); // split each song into its attributes
+
+        if (song.length != 23){ 
+            continue;
+        }
+
         songObj = { // assign each attribute 
             track_id: i,
             track_name: song[1],
             track_artist: song[2],
             track_album_name: song[5], 
             playlist_name: song[7], 
-            playlist_genre: song[9], 
-            playlist_subgenre: song[10],  
+            genre: {playlist_genre: song[9], 
+                playlist_subgenre: song[10]}, 
             duration: parseInt(song[22])
         }
     
@@ -33,10 +38,9 @@ function convertDataToSong(filename){
     const seconds = ((songObj.duration % 60000) / 1000).toFixed(0);
     songObj.duration = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 
+    genre_array.push(songObj.genre)
     song_array.push(songObj)
     }
-
-
 
     return song_array;
 }
