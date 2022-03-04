ffe6f84ee3729477225d9f5dbd29e927cccb61bb
Mia Nguyen <quynhnhu11579@gmail.com>
2022-03-04 10:51:36 -0330

    Add client credential

---
+++ Iter Two/api/credential.js
@@ -0,0 +1,43 @@
+/**
+ * A basic node.js script that performs the Client Credentials 
+ * oAuth2 flow to authenticate against the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+ */
+
+ var request = require('request'); // "Request" library
+
+ var client_id = '6dcf3051ee9842b78f2d130f8464d9dd'; // Your client id
+ var client_secret = '059c3e2748f546aa923a0ed54b1b5e47'; // Your secret
+ 
+ // your application requests authorization
+ var authOptions = {
+   url: 'https://accounts.spotify.com/api/token',
+   headers: {
+     'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+   },
+   form: {
+     grant_type: 'client_credentials'
+   },
+   json: true
+ };
+ 
+ request.post(authOptions, function(error, response, body) {
+   if (!error && response.statusCode === 200) {
+ 
+     // use the access token to access the Spotify Web API
+     var token = body.access_token;
+     var options = {
+       url: 'https://api.spotify.com/v1/users/31lmjmqtzgmennebs7vslcfxm5d4',
+       headers: {
+         'Authorization': 'Bearer ' + token
+       },
+       json: true
+     };
+     request.get(options, function(error, response, body) {
+       console.log(body);
+     });
+   }
+ });
+ 
