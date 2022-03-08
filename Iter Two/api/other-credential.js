561ef9c03956acad73574a3c821af8d7e5331530
Mia Nguyen <quynhnhu11579@gmail.com>
2022-03-08 19:19:18 -0330

    Fix credentials

--- Iter Two/api/other-credential.js
+++ Iter Two/api/other-credential.js
@@ -19,15 +19,14 @@ const spotifyApi = new SpotifyWebApi({
     redirectUri: 'http://localhost:8888/callback',
   });
 
-  
-getUserInfo();
-getUserPlaylists();
+async function main () {
+    await getUserInfo()
+    await getUserPlaylists()
+}
+main()
 
 spotifyApi.clientCredentialsGrant()
 .then(function(data) {
-    //console.log('The access token expires in ' + data.body['expires_in']);
-    //console.log('The access token is ' + data.body['access_token']);
-  
     // Save the access token so that it's used in future calls
     spotifyApi.setAccessToken(data.body['access_token']);
 
