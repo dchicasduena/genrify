41f76968e68b184100262db8323efb0dbd2b05f6
David Chicas <dchicasduena@mun.ca>
2022-03-16 17:45:28 -0230

    import 2

--- Iter Two/import2.js
+++ Iter Two/import2.js
@@ -59,57 +59,17 @@ async function importData() {
       }
     });
 
-  // CSV file import
-  const csv = 'data/spotify_songs.csv';
-  var arrayToInsert = [];
-  await csvtojson().fromFile(csv).then(source => {
+  
     // Insert into the table
     var collectionName = 'all_songs';
     var collection = dbConn.collection(collectionName);
     dbConn.listCollections({ name: collectionName })
       .next(async function (err, collinfo) {
         if (!collinfo) { // If the collection does not exist, create it
-          // Fetching the all data from each row
-          for (var i = 0; i < source.length; i++) {
-            var oneRow = {
-              track_id: 'spotify:track:' + source[i].track_id,
-              track_name: source[i].track_name,
-              track_artist: source[i].track_artist,
-              track_popularity: source[i].track_popularity,
-              track_album_id: 'spotify:album:' + source[i].track_album_id,
-              track_album_name: source[i].track_album_name,
-              track_album_release_date: source[i].track_album_release_date,
-              playlist_name: source[i].playlist_name,
-              playlist_id: source[i].playlist_id,
-              playlist_genre: source[i].playlist_genre,
-              playlist_subgenre: source[i].playlist_subgenre,
-              danceability: source[i].danceability,
-              energy: source[i].energy,
-              key: source[i].key,
-              loudness: source[i].loudness,
-              mode: source[i].mode,
-              speechiness: source[i].speechiness,
-              acousticness: source[i].acousticness,
-              instrumentalness: source[i].instrumentalness,
-              liveness: source[i].liveness,
-              valence: source[i].valence,
-              tempo: source[i].tempo,
-              duration_ms: source[i].duration_ms,
-            };
-            arrayToInsert.push(oneRow);
-          }
-          // await collection.insertMany(arrayToInsert, (err, result) => {
-          //   if (err) console.log(err);
-          // if (result) {
-          //   console.log('Import CSV into database successfully.');
-          //}
-          //});
-
           // Arrange the data
           await arrangeData();
         }
       });
-  });
 }
 
 async function arrangeData() {
@@ -137,13 +97,6 @@ async function arrangeData() {
     }
   }
 
-  var collection = dbConn.collection('all_songs');
-  await collection.insertMany(arrayToInsert, (err, result) => {
-    if (err) console.log(err);
-    if (result) {
-      console.log('Arrange spotify data successfully. You can now close the terminal.');
-    }
-  });
 }
 
 // your application requests authorization
@@ -224,6 +177,8 @@ function fixGenre(artistGenres, type) {
 
   return mainList;
 }
+
+
 
 async function main() {
   ALL_GENRES = await getAllGenres();
