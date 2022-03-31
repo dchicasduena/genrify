81124aecf5899703bd5389665a51d70b50cd8d06
Mia Nguyen <quynhnhu11579@gmail.com>
2022-03-31 16:06:59 -0230

    update

--- random.js
+++ random.js
@@ -123,7 +123,7 @@ module.exports.createPlaylist = async (req, res) => {
             playlist_subgenre: playlist[i].playlist_subgenre
         });
     }
-    res.send('playlist created');
+    res.send(playlist);
 }
 
 module.exports.getPlaylist = async (req, res) => {
@@ -142,99 +142,6 @@ module.exports.getPlaylist = async (req, res) => {
     let del = await _del_playlist_collection();
 }
 
-// get favorite genre of user
-async function getUserGenre() {
-    const readline = require("readline");
-
-    function askQuestion(query) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-        return new Promise(resolve => rl.question(query, ans => {
-            rl.close();
-            resolve(ans);
-        }))
-    }
-
-    // print all genres 
-    console.log('List of all genres:')
-    for (let i = 0; i < genreList.length; i++) {
-        console.log(genreList[i])
-    }
-
-    // get favorite genres
-    while (userGenre.length < 3) {
-        let ans = await askQuestion("What is your favorite genre? (" + (3 - userGenre.length) + " left) ")
-        if (userGenre.includes(ans)) { // if user has already selected the genre, skip it
-            console.log("You have already selected this genre")
-            continue
-        }
-        if (!genreList.includes(ans)) { // if user has selected an invalid genre, skip it
-            console.log("Invalid genre")
-            continue
-        }
-        userGenre.push(ans)
-    }
-
-    // print all relevant subgenres
-    console.log('List of relevant subgenres:')
-    for (let i = 0; i < userGenre.length; i++) {
-        console.log(userGenre[i])
-        let subGenre = getSubGenre(userGenre[i]);
-        for (let j = 0; j < subGenre.length; j++) {
-            console.log(" - " + subGenre[j])
-        }
-    }
-
-    // get possible subgenres
-    let possibleSubGenre = [];
-    for (let i = 0; i < userGenre.length; i++) {
-        let subGenre = getSubGenre(userGenre[i]);
-        for (let j = 0; j < subGenre.length; j++) {
-            possibleSubGenre.push(subGenre[j])
-        }
-    }
-
-    // get favorite subgenres
-    console.log('Enter -1 to stop entering subgenres.')
-    while (userSubgenre.length < 6) {
-        let ans = await askQuestion("What is your favorite subgenre? (" + (6 - userSubgenre.length) + " left).")
-        if (ans == -1) { // if user wants to stop entering subgenres, break
-            if (userSubgenre.length < 3) {
-                console.log("You must select at least 3 subgenres.")
-                continue
-            }
-            break
-        }
-        if (!possibleSubGenre.includes(ans)) { // if user has selected an invalid subgenre, skip it
-            console.log("Invalid subgenre")
-            continue
-        }
-        if (userSubgenre.includes(ans)) { // if user has already selected the genre, skip it
-            console.log("You have already selected this subgenre")
-            continue
-        }
-        userSubgenre.push(ans) // add subgenre to userSubgenre
-    }
-
-    // this part needs to be inside this function for it to work, am trying to think of another way
-    this.userGenre = userGenre;
-    this.userSubgenre = userSubgenre;
-
-    // get number of recommended songs
-    let ans = 0;
-    while (ans < 10 || ans > 50) {
-        ans = await askQuestion("How many songs would you like to have in your playlist? (10-50) ")
-        if (ans < 10 || ans > 50) {
-            console.log("Invalid number, please try again")
-        } else {
-            break
-        }
-    }
-
-    createPlaylist(ans);
-}
 
 // get all subgenres of a genre
 function getSubGenre(genre, songs) {
--- view/js/playlist.js
+++ view/js/playlist.js
@@ -13,10 +13,15 @@ $(document).ready(function () {
             type: 'GET',
             contentType: 'application/json',
             success: function (response) { // not returning a response rn, but playlist is imported to mongo
-                console.log(response);
-                for (i = 0; i > response.length; i++){
-                    let song = response[i]['track_name'] + ' - ' + response[i].artist;
-                    $('#playlist').append(song);
+                // console.log(response);
+                for (i = 0; i < response.length; i++){
+                    let song = response[i].track_name + ' - ' + response[i].track_artist;
+                    // console.log(song);
+                    $('#playlist').append(
+                        $(document.createElement('p5')).prop({
+                            type: 'p5',
+                            innerHTML: song + '<br>'
+                        }));
                 };
             },
             // If there's an error, we can use the alert box to make sure we understand the problem
