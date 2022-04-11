e950859c6bc6792e983a563af1ed9955f96852fb
David Chicas <dchicasduena@mun.ca>
2022-04-11 19:27:03 -0230

    trying more heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 * @course COMP 3100 - Web Programming
 * @year 2022 
 */
-
+const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const express = require('express');
 const app = express();
